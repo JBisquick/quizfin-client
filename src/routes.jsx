83d0f0ac69cdafa2c5fb43ba6b23b3ae611fe42b
JBisquick <felipebirch@gmail.com>
2024-08-27 16:10:53 -0700

    Add app file with routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,12 @@
+import App from "./App";
+import ErrorPage from "./errorPage";
+
 const routes = [
   {
-
+    path: "/",
+    element: <App/>,
+    errorElment: <ErrorPage/>
   }
-]
+];
 
 export default routes;
