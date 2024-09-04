4eac2e3275a86d8992f429402b653b79246000ca
JBisquick <felipebirch@gmail.com>
2024-09-04 12:37:59 -0700

    Add a loading state to app, so children components will wait for user information to be fetched

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,24 @@
 import { Outlet } from 'react-router-dom';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useRefreshToken from '../hooks/useRefreshToken';
 import useAuth from '../hooks/useAuth';
 import Navigation from './Navigation';
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
   const refresh = useRefreshToken();
   const { auth } = useAuth();
 
   useEffect(() => {
     const verifyRefreshToken = async () => {
       await refresh();
+      setLoading(false);
     };
 
     if (!auth?.accessToken) verifyRefreshToken();
   }, []);
+
+  if (loading) return <div>Loading...</div>
 
   return (
     <div>
