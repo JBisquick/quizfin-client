d2fa5d2b1f3e0e164343c4bfa3328d2496e1bf78
JBisquick <felipebirch@gmail.com>
2024-09-13 16:04:27 -0700

    Add styling to login and sign up page

---
+++ src/pages/auth/Login.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from '../../api/axios';
+import useAuth from '../../hooks/useAuth';
+import styles from './auth.module.css';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const { setAuth } = useAuth();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        '/auth/login',
+        JSON.stringify({ username, password })
+      );
+      if (response.status >= 400) {
+        throw new Error('Server Error');
+      }
+      const data = response.data;
+
+      setPassword('');
+      if (data?.errors) {
+        setMessage(data.errors);
+      } else {
+        setAuth({
+          username: data.username,
+          accessToken: data.accessToken,
+          id: data.id
+        });
+        navigate('/');
+      }
+    } catch (err) {
+      setMessage([{ msg: 'No Server Response' }]);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Login</h2>
+      <form onSubmit={handleSubmit} className={styles.form_container}>
+        <input
+          className={styles.input}
+          type="text"
+          id="username"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          className={styles.input}
+          type="password"
+          id="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit" className={styles.button}>
+          Login
+        </button>
+      </form>
+      {message.map((error) => (
+        <p key={error.msg} className={styles.error}>
+          {error.msg}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/auth/Signup.jsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from '../../api/axios';
+import styles from './auth.module.css';
+
+const Signup = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConf, setPasswordConf] = useState('');
+  const [message, setMessage] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        '/auth/signup',
+        JSON.stringify({
+          username,
+          email,
+          password,
+          passwordConfirmation: passwordConf
+        })
+      );
+      if (response.status >= 400) {
+        throw new Error('Server Error');
+      }
+      const data = response.data;
+
+      setPassword('');
+      setPasswordConf('');
+      if (data?.errors) {
+        setMessage(data.errors);
+      } else {
+        navigate('/login');
+      }
+    } catch (err) {
+      setMessage([{ msg: 'No Server Response' }]);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Sign Up</h2>
+      <form onSubmit={handleSubmit} className={styles.form_container}>
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          className={styles.input}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          className={styles.input}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className={styles.input}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          id="passwordConf"
+          placeholder="Password Confirmation"
+          className={styles.input}
+          value={passwordConf}
+          onChange={(e) => setPasswordConf(e.target.value)}
+          required
+        />
+        <button type="submit" className={styles.button}>
+          Sign Up
+        </button>
+      </form>
+      {message.map((error) => (
+        <p key={error.msg} className={styles.error}>
+          {error.msg}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Signup;
