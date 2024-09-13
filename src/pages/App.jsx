81c70bb731944b774a678795a70ed9dac10a7605
JBisquick <felipebirch@gmail.com>
2024-09-13 14:24:14 -0700

    Start basic to website

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -2,7 +2,8 @@ import { Outlet } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import useRefreshToken from '../hooks/useRefreshToken';
 import useAuth from '../hooks/useAuth';
-import Navigation from './Navigation';
+import Navigation from '../components/Navigation';
+import './App.css';
 
 const App = () => {
   const [loading, setLoading] = useState(true);
