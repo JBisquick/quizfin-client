9e37d5bbe5cd36b8cd8c1f8ab2ab5a741bf5dd3b
JBisquick <felipebirch@gmail.com>
2024-09-18 14:48:29 -0700

    Remove email from sign up form

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -6,7 +6,6 @@ import styles from './auth.module.css';
 const Signup = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConf, setPasswordConf] = useState('');
   const [message, setMessage] = useState([]);
@@ -18,7 +17,6 @@ const Signup = () => {
         '/auth/signup',
         JSON.stringify({
           username,
-          email,
           password,
           passwordConfirmation: passwordConf
         })
@@ -52,18 +50,6 @@ const Signup = () => {
             className={styles.input}
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            autocomplete="off"
-            required
-          />
-        </div>
-        <div className={styles.input_container}>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            className={styles.input}
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
             autocomplete="off"
             required
           />
